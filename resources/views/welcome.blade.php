478a01ee8aab404a0bfca11dd29bae40eef430ef
SawsanT <aljuhanisawsan@gmail.com>
2023-11-18 22:45:39 +0300

    Edit Home Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,238 @@
-@extends("layouts.master")
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <meta content="فهيم AI Team" name="author">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="خاصية البحث العكسي في معجم الرياض، بحيث يتاح للمستخدم إدخال وصف للكلمة أو شرحها أو معناها، ويقوم نموذج الذكاء الاصطناعي باقتراح عدد من المفردات التي تؤدي نفس المعنى في الوصف، بالإضافة إلى توليد صورة لهذه المفردة" name="description">
+    <meta content="ai,image,description,meaning,words,dictionary" name="keywords">
+
+
+    <title>فهيم</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet"
+        type="text/css" />
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet"
+        type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href=" {{ asset('/css/style-landingpage.css') }} " rel="stylesheet" />
+
+    <style>
+    h1 {
+        font-size: 5rem !important;
+    }
+
+    .masthead p{
+        font-size: 1.5rem !important;
+
+    }
+    </style>
+</head>
+
+<body>
+    <!-- Navigation-->
+    <nav class="navbar navbar-light bg-light static-top">
+        <div class="container">
+            <a class="navbar-brand" href="/"><img src="/img//fahim.png" width="40%" class="img-fluid rounded-top"
+                    alt=""></a>
+            <a class="btn btn-primary" href="/chat">تحدث مع فهيم</a>
+        </div>
+    </nav>
+    <!-- Masthead-->
+    <header class="masthead">
+        <div class="container position-relative">
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-xs-12">
+                    <div class="text-center text-white ">
+                        <!-- Page heading-->
+                        <h1 class="mb-5">فهيم</h1>
+                        <p> 
+                        فكرة مشروعنا تتمثل في إضافة خاصية البحث العكسي في معجم الرياض، بحيث يتيح للمستخدم إدخال وصف للكلمة أو شرحها أو معناها، ويقوم نموذج الذكاء الاصطناعي بتحليل الوصف المدخل وتوليد المفردة التي تؤدي نفس المعنى، بالإضافة إلى توليد صورة لهذه المفردة
+
+                        </p>
+
+                    </div>
+
+
+                </div>
+
+                <div class="col-md-6 col-xs-12 text-center">
+                    <!-- Page heading-->
+                    <img src="/img/real-life-chat3.png" width="65%" style="border-radius: 100px" alt="">
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Icons Grid-->
+    <section class="features-icons bg-light text-center">
+        <div class="container">
+            <div class="row">
+                <h2 style="padding: 34px">
+                أثره في إثراء منظومة معجم الرياض
+
+                </h2>
+                <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                        <div class="features-icons-icon d-flex"><i class="bi-translate  m-auto text-primary"></i></div>
+                        <h4>خدمة الناطقين بغير العربية</h4>
+                       
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                        <div class="features-icons-icon d-flex"><i class="bi-card-image m-auto text-primary"></i></div>
+                        <h4>ربط المعاني بالصور لتحديدها على وجه الدقّة
+</h4>
+
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+                        <div class="features-icons-icon d-flex"><i class=" bi-hand-index-thumb m-auto text-primary"></i></div>
+                        <h4>تيسير الوصول إلى المراد وتسهيله على المستخدم
+</h4>
+                        <!-- <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="features-icons bg-light text-center" style="background-color: #52CBBE !important;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                        <div class="features-icons-icon d-flex"><i class="bi-clipboard-data m-auto text-primary"></i>
+                        </div>
+                        <h3>البيانات المستخدمة</h3>
+                        <p class="lead mb-0">
+                            تم استخدام ١٠٠٠ سجل من معجم الرياض لتدريب النموذج
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                        <div class="features-icons-icon d-flex"><i class="bi-terminal m-auto text-primary"></i></div>
+                        <h3>التقنيات المستخدمة</h3>
+                        <p class="lead mb-0" style="direction: ltr;">
+                        Laravel,                        MultiRD, <br>
+                            Multi-channel Reverse Dictionary Model,
+                            
+                            Dall-e +
+                        </p>
+                    </div>
+                </div>
+                <!-- <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+                        <div class="features-icons-icon d-flex"><i class="bi-terminal m-auto text-primary"></i></div>
+                        <h3>Easy to Use</h3>
+                        <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
+                    </div>
+                </div> -->
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Image Showcases-->
+    <!-- <section class="showcase">
+        <div class="container-fluid p-0">
+            <div class="row g-0">
+                <div class="col-lg-6 order-lg-2 text-white showcase-img"
+                    style="background-image: url('assets/img/bg-showcase-1.jpg')"></div>
+                <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                    <h2>Fully Responsive Design</h2>
+                    <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will
+                        look great on any device, whether it's a phone, tablet, or desktop the page will behave
+                        responsively!</p>
+                </div>
+            </div>
+            <div class="row g-0">
+                <div class="col-lg-6 text-white showcase-img"
+                    style="background-image: url('assets/img/bg-showcase-2.jpg')"></div>
+                <div class="col-lg-6 my-auto showcase-text">
+                    <h2>Updated For Bootstrap 5</h2>
+                    <p class="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 5 is leading the
+                        way in mobile responsive web development! All of the themes on Start Bootstrap are now using
+                        Bootstrap 5!</p>
+                </div>
+            </div>
+            <div class="row g-0">
+                <div class="col-lg-6 order-lg-2 text-white showcase-img"
+                    style="background-image: url('assets/img/bg-showcase-3.jpg')"></div>
+                <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                    <h2>Easy to Use & Customize</h2>
+                    <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand
+                        some deeper customization options. Out of the box, just add your content and images, and your
+                        new landing page will be ready to go!</p>
+                </div>
+            </div>
+        </div>
+    </section> -->
+    <!-- Testimonials-->
+    <!-- <section class="testimonials text-center bg-light">
+        <div class="container">
+            <h2 class="mb-5">What people are saying...</h2>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                        <img class="img-fluid rounded-circle mb-3" src="assets/img/testimonials-1.jpg" alt="..." />
+                        <h5>Margaret E.</h5>
+                        <p class="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                        <img class="img-fluid rounded-circle mb-3" src="assets/img/testimonials-2.jpg" alt="..." />
+                        <h5>Fred S.</h5>
+                        <p class="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of
+                            super nice landing pages."</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                        <img class="img-fluid rounded-circle mb-3" src="assets/img/testimonials-3.jpg" alt="..." />
+                        <h5>Sarah W.</h5>
+                        <p class="font-weight-light mb-0">"Thanks so much for making these free resources available to
+                            us!"</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+    <!-- Call to Action-->
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05); ">
+    © 
+    <a class="text-reset fw-bold" href="/"> فهيم 2023</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <!-- * *                               SB Forms JS                               * *-->
+    <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+</body>
+
+</html>
